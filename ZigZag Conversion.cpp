c18f724a5d05b911f0690f0ab86488987da5240e
lxtlxt1045 <lxt1045@gmail.com>
2015-08-12 11:05:06 +0800

    ZigZag Conversion
    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    
    P   A   H   N
    A P L S I I G
    Y   I   R
    And then read line by line: "PAHNAPLSIIGYIR"
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string text, int nRows);
    convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".

---
+++ ZigZag Conversion.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int lenth = s.size();
+		if (lenth <= 2 || numRows == 1) return s;
+		char *p = (char *)malloc(lenth);
+		int n = 0, m = 0;//n：行号；m：列号,第几列。
+		int cycle = numRows + numRows - 2;
+		int numCols = lenth / cycle + 1;//总列数
+		for (int i = 0; i < lenth;){
+			if (n == 0 || n == numRows - 1){//短行
+				int index = m*cycle + n;
+				if (index >= lenth){
+					n++;
+					m = 0;
+					continue;
+				}
+				p[i++] = s[index];// [m*cycle + n];
+			}
+			else{//长行
+				int index = m*cycle + n;
+				if (index >= lenth){
+					n++;
+					m = 0;
+					continue;
+				}
+				p[i++] = s[index];// [m*cycle + n];
+				index = m*cycle + numRows - 1 + numRows - 1 - n;
+				if (index >= lenth){
+					n++;
+					m = 0;
+					continue;
+				}
+				p[i++] = s[index];
+			}
+
+			if (++m >= numCols){
+				m = 0;
+				n++;
+			}
+		}
+		for (int i = 0; i < lenth; i++){
+			s[i] = p[i];
+		}
+		free(p);
+		return s;
+    }
+};
