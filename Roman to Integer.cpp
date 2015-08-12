12936900c3b022b98d7356e68f548f506cd3a3ce
lxtlxt1045 <lxt1045@gmail.com>
2015-08-12 11:11:54 +0800

    Roman to Integer
    Given a roman numeral, convert it to an integer.
    
    Input is guaranteed to be within the range from 1 to 3999.

---
+++ Roman to Integer.cpp
@@ -0,0 +1,125 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+    //     register int tmp = 0;
+    // 	for (int i = 0; ; i++){
+    // 		/*switch (s[i])
+    // 		{
+    // 		case 'I'://1
+    // 			tmp += 1;
+    // 			break;
+    // 		case 'V'://5
+    // 			tmp += 5;
+    // 			if (i> 0 && s[i - 1] == 'I')
+    // 				tmp -= 2;
+    // 			break;
+    // 		case 'X'://10
+    // 			tmp += 10;
+    // 			if (i> 0 && s[i - 1] == 'I')
+    // 				tmp -= 2;
+    // 			break;
+    // 		case 'L'://50
+    // 			tmp += 50;
+    // 			if (i> 0 && s[i - 1] == 'X')
+    // 				tmp -= 20;
+    // 			break;
+    // 		case 'C'://100
+    // 			tmp += 100;
+    // 			if (i> 0 && s[i - 1] == 'X')
+    // 				tmp -= 20;
+    // 			break;
+    // 		case 'D'://500
+    // 			tmp += 500;
+    // 			if (i> 0 && s[i - 1] == 'C')
+    // 				tmp -= 200;
+    // 			break;
+    // 		case 'M'://1000
+    // 			tmp += 1000;
+    // 			if (i> 0 && s[i - 1] == 'C')
+    // 				tmp -= 200;
+    // 			break;
+    // 		default:
+    // 			 return tmp;
+    // 		}//*/
+    // 		if (s[i] == 'I'){//1
+    // 			tmp += 1;
+    // 		}
+    // 		else if (s[i] == 'V'){//5
+    // 			tmp += 5;
+    // 			if (i> 0 && s[i - 1] == 'I')
+    // 				tmp -= 2;
+    // 		}
+    // 		else if (s[i] == 'X'){//10
+    // 			tmp += 10;
+    // 			if (i> 0 && s[i - 1] == 'I')
+    // 				tmp -= 2;
+    // 		}
+    // 		else if (s[i] == 'L'){//50
+    // 			tmp += 50;
+    // 			if (i> 0 && s[i - 1] == 'X')
+    // 				tmp -= 20;
+    // 		}
+    // 		else if (s[i] == 'C'){//100
+    // 			tmp += 100;
+    // 			if (i> 0 && s[i - 1] == 'X')
+    // 				tmp -= 20;
+    // 		}
+    // 		else if (s[i] == 'D'){//500
+    // 			tmp += 500;
+    // 			if (i> 0 && s[i - 1] == 'C')
+    // 				tmp -= 200;
+    // 		}
+    // 		else if (s[i] == 'M'){//1000
+    // 			tmp += 1000;
+    // 			if (i> 0 && s[i - 1] == 'C')
+    // 				tmp -= 200;
+    // 		}
+    // 		else if (s[i] == 0){//1000
+    // 		    return tmp;
+    // 		}//*/
+    // 	}
+    int tmp = 0;
+	register char *sP = (char *)(&s[0]);
+	do{
+		switch (*sP++)
+		{
+		case 'I'://1
+			tmp += 1;
+			break;
+		case 'V'://5
+			if (tmp != 0 && sP[-2] == 'I')
+				tmp -= 2;
+			tmp += 5;
+			break;
+		case 'X'://10
+			if (tmp != 0 && sP[-2] == 'I')
+				tmp -= 2;
+			tmp += 10;
+			break;
+		case 'L'://50
+			if (tmp != 0 && sP[-2] == 'X')
+				tmp -= 20;
+			tmp += 50;
+			break;
+		case 'C'://100
+			if (tmp != 0 && sP[-2] == 'X')
+				tmp -= 20;
+			tmp += 100;
+			break;
+		case 'D'://500
+			if (tmp != 0 && sP[-2] == 'C')
+				tmp -= 200;
+			tmp += 500;
+			break;
+		case 'M'://1000
+			if (tmp != 0 && sP[-2] == 'C')
+				tmp -= 200;
+			tmp += 1000;
+			break;
+		default:
+			return tmp;
+		}
+	} while (true);
+    	return tmp;
+    }
+};
