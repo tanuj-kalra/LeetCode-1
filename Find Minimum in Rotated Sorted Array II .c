db23b6a52749539ffe6679b2cc6a83fddce6505e
lxtlxt1045 <lxt1045@gmail.com>
2015-08-12 11:02:34 +0800

    Find Minimum in Rotated Sorted Array II
    Follow up for "Find Minimum in Rotated Sorted Array":
    What if duplicates are allowed?
    
    Would this affect the run-time complexity? How and why?
    Suppose a sorted array is rotated at some pivot unknown to you beforehand.
    
    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
    
    Find the minimum element.
    
    The array may contain duplicates.

---
+++ Find Minimum in Rotated Sorted Array II .c
@@ -0,0 +1,43 @@
+int findMin(int* nums, int numsSize) {
+    int left = 0, right = numsSize-1, pivot = right / 2;
+		while (right-left > 1){
+			if (nums[pivot] > nums[left]){
+				if (nums[right] >= nums[pivot])
+					return nums[left];
+				left = pivot;
+				pivot = (left + right) / 2;
+			}
+			else if (nums[pivot] == nums[left]){
+				while (++left < pivot && nums[left] == nums[pivot]);
+				if (left<pivot)
+					return nums[left]<nums[pivot] ? nums[left] : nums[pivot];
+					/*if(nums[left]<nums[pivot]) return nums[left];
+					else return nums[pivot];*/
+			/*	while (++left < pivot){
+				    if(nums[left] == nums[pivot]) continue;
+				    else if(nums[left] < nums[pivot]) return nums[left];
+				    else return nums[pivot];
+				}*/
+				left = pivot;
+				pivot = (left + right) / 2;
+			}
+			else{
+				right = pivot;
+				pivot = (left + right) / 2;
+			}
+		}
+		//pivot = nums[pivot] <= nums[left] ? pivot : left;
+		//return nums[pivot] <= nums[right] ? nums[pivot] : nums[right];
+		if(nums[pivot] <= nums[left]){
+		    if(nums[pivot] <= nums[right])
+		        return nums[pivot];
+		    else
+		        return nums[right];
+		}
+		else{
+		    if(nums[left] <= nums[right])
+		        return nums[left];
+		    else
+		        return nums[right];
+		}
+}
