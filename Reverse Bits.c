8a8ecfc967fa8e3b1bce07438e7ca36723a78a75
lxtlxt1045 <lxt1045@gmail.com>
2015-08-12 11:09:38 +0800

    Reverse Bits
    Reverse bits of a given 32 bits unsigned integer.
    
    For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
    
    Follow up:
    If this function is called many times, how would you optimize it?
    
    Related problem: Reverse Integer
    
    Credits:
    Special thanks to @ts for adding this problem and creating all test cases.

---
+++ Reverse Bits.c
@@ -0,0 +1,14 @@
+uint32_t reverseBits(uint32_t n) {
+    register uint32_t sum = 0;
+	/*for (int i = 0; i <= 15; i++){
+		//sum += ((n >> i)& 1) << (31 - i);
+		//sum += ((n << i)&(1<<31)) >> (31 - i);
+		sum |= ((n << 31 - i) >> 31) << (31 - i);
+		sum |= ((n >> 31 - i) << 31) >> (31 - i);
+	}//*/
+	for (register uint32_t i = 1,j=1<<31; i >0; i = i << 1,j = j>>1){
+		if (n & i)
+			sum |= j;
+	}//*/
+	return sum;
+}
