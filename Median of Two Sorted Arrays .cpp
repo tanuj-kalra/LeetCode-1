d43879aabab7ecb1edaa4251457086443cfb96a4
Tanuj Kalra <67724065+tanuj-kalra@users.noreply.github.com>
2020-10-01 12:55:47 +0530

    Update Median of Two Sorted Arrays .cpp

--- Median of Two Sorted Arrays .cpp
+++ Median of Two Sorted Arrays .cpp
@@ -135,3 +135,44 @@ public:
 		}//*/
     }
 };
+
+// I want to contribute a Binary Search Short Code (Alternative).
+Kindly Acknowledge
+double Solution::findMedianSortedArrays(const vector<int> &nums1, const vector<int> &nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+        if(m>n) return findMedianSortedArrays(nums2,nums1);
+        
+        int low = 0;
+        int high = m;
+        double ans;
+        while(low<=high){
+            int partitionX = (low+high)/2;
+            int partitionY = (m+n+1)/2 - partitionX;
+            
+            int maxLeftX =  partitionX==0?INT_MIN:nums1[partitionX-1];
+            int minRightX = partitionX==m?INT_MAX:nums1[partitionX];
+            
+            int maxLeftY =  partitionY==0?INT_MIN:nums2[partitionY-1];
+            int minRightY = partitionY==n?INT_MAX:nums2[partitionY];
+            
+            if(maxLeftX<=minRightY && maxLeftY<=minRightX){
+                if((m+n)%2==0)
+                {
+                   double temp1 = max(maxLeftX,maxLeftY);
+                   double temp2 = min(minRightX,minRightY);
+                   ans = (temp1+temp2)/(2*1.0);
+                   break;
+                }
+                else{
+                    ans = max(maxLeftX,maxLeftY)*1.0;
+                    break;
+                }
+            }
+            else if(maxLeftX>minRightY){
+                high = partitionX-1;
+            }
+            else low = partitionX+1;
+        }
+        return ans;
+}
